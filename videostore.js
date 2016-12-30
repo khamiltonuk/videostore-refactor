14029f841688baec39f4fbb0ab1e9cab5ce57415
krihamilton <krihamilton@gumtree.com>
2016-12-30 14:27:21 +0000

    refactor variable name to better reason over code

--- videostore.js
+++ videostore.js
@@ -32,27 +32,27 @@ function statement(customer, movies) {
   }
 
   function amountFor(r){
-    let thisAmount = 0;
+    let result = 0;
 
       // determine amount for each movie
       switch (movieFor(r).code) {
         case "regular":
-          thisAmount = 2;
+          result = 2;
           if (r.days > 2) {
-            thisAmount += (r.days - 2) * 1.5;
+            result += (r.days - 2) * 1.5;
           }
           break;
         case "new":
-          thisAmount = r.days * 3;
+          result = r.days * 3;
           break;
         case "childrens":
-          thisAmount = 1.5;
+          result = 1.5;
           if (r.days > 3) {
-            thisAmount += (r.days - 3) * 1.5;
+            result += (r.days - 3) * 1.5;
           }
           break;
       }
-      return thisAmount;
+      return result;
   }
 
   function movieFor(rental) {return movies[rental.movieID];}
